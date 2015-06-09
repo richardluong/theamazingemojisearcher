f7391468baa8cedf5c8a303905108663a7890d60
Richard Luong <richardluong92@gmail.com>
2015-06-09 16:55:42 +0800

    Soon finished

--- server.py
+++ server.py
@@ -14,6 +14,7 @@ from constants import DOCUMENT_FOLDER, INDEX_DIR
 
 import sys
 import logging
+from random import randint
 
 app = Flask(__name__)
 ix = index.open_dir(INDEX_DIR)
@@ -24,19 +25,12 @@ app.logger.setLevel(logging.ERROR)
 def start():
     not_found = request.args.get('not_found', '')
     q = request.args.get('q', '')
-    if q == "":
-        return render_template('index.html')
-    else:
-        return render_template("index.html", results=query(q), query=q)
+    return render_template("index.html", results=query(q), query=q, rand_emoji=rand_emoji())
 
 @app.route('/emoji/<index>')
 def emojis(index):
-    try:
-        with open(join(DOCUMENT_FOLDER, index)) as document:
-            emoji = pickle.load(document)
-        return render_template("single.html", emoji=emoji)
-    except IOError, e:
-        return redirect(url_for('start', not_found=1))
+    return render_template("single.html", emoji=get_emoji(index))
+    
 
 def query(querystring):
     parser = MultifieldParser(["name", "annotations", "description", "related_to", "known_as"], ix.schema, termclass=Variations)
@@ -44,5 +38,18 @@ def query(querystring):
     searcher = ix.searcher()
     return searcher.search(myquery)
 
+def get_emoji(index):
+    try:
+        with open(join(DOCUMENT_FOLDER, index)) as document:
+            emoji = pickle.load(document)
+            return emoji
+    except IOError, e:
+        return redirect(url_for('start', not_found=1))
+
+def rand_emoji():
+    rand = randint(1, 1281)
+    return get_emoji(str(rand))
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
